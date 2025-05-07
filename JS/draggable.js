34ac26f06634b009d381108ed1c6b8678beb1f89
CormacKeane <G00429254@atu.ie>
2025-05-07 18:30:52 +0100

    draggable boxes work and record what is selected

--- JS/draggable.js
+++ JS/draggable.js
@@ -1,15 +1,19 @@
+//selection boxes
 const box = document.querySelectorAll('.selectionBox');
 let draggedElement=null;
+//dropzones
 const dropZones = document.querySelectorAll('section');
+//og spot
+const boxCon=document.querySelector('.box-container');
 
 //assigning drag to boxes
 box.forEach((el) => {
-    box.ondragstart = function(e) {
+    el.ondragstart = function(e) {
         draggedElement = e.target;
         this.style.opacity = '0.5';
     }
 
-    box.ondragend = function(e) {
+    el.ondragend = function(e) {
         this.style.opacity = '1';
     }
 })
@@ -30,15 +34,45 @@ dropZones.forEach(zone => {
         e.preventDefault();
         this.classList.remove('over');
 
-        // Only allow drop if the zone is empty
-        /*if (this.children.length === 0 && draggedElement) {
+        // Only allow drop if the dropzone's empty
+        if (this.children.length === 0 && draggedElement) {
             this.appendChild(draggedElement);
         } else {
             alert("one box per zone!")
-        }*/
+        }
     });
 });
 
+//allows reseting box to og spot
+boxCon.addEventListener('dragover', function (e) {
+    e.preventDefault();
+});
+
+boxCon.addEventListener('drop', function (e) {
+    e.preventDefault();
+    if(draggedElement) {
+        this.appendChild(draggedElement);
+    }
+})
 
 
+function whatGenres() {
+    let zone1 = null;
+    let zone2 = null;
 
+    for (let i = 0; i < dropZones.length; i++) {
+        const zone = dropZones[i];
+
+        if (zone.children.length > 0) {
+            if (i === 0) {
+                zone1 =zone.children[0].textContent
+            } else if (i === 1) {
+                zone2 =zone.children[0].textContent
+            }
+        } else {
+            console.log("Dropzone "+(i + 1)+" is empty");
+        }
+    }
+
+    console.log(zone1+" and "+zone2);
+}
